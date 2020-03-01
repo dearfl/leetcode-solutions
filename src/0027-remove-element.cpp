f8c9938a5e820390f52ee3f9e1a053a44b030f72
dearfl <i@flr.me>
2020-03-01 08:50:30 +0800

    add a solution of p27

--- src/0027-remove-element.cpp
+++ src/0027-remove-element.cpp
@@ -51,3 +51,16 @@ public:
     }
 };
 
+// cleaner solution without std
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != val) {
+                nums[cnt++] = nums[i];
+            }
+        }
+        return cnt;
+    }
+}
