2042916f7c9ddd830a334c893809107eddb308ff
dearfl <i@flr.me>
2021-08-14 11:55:22 +0800

    1346 Check if N and it's Double Exist

---
+++ src/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,22 @@
+// Given an array arr of integers, check if there exists two integers N and M such that N is the double of M ( i.e. N = 2 * M).
+//
+// More formally check if there exists two indices i and j such that :
+//
+// i != j
+// 0 <= i, j < arr.length
+// arr[i] == 2 * arr[j]
+//
+
+ass Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for (const auto &i : arr) {
+            if (s.find(i * 2) != s.end() || i % 2 == 0 && s.find(i / 2) != s.end()) {
+                return true;
+            }
+            s.insert(i);
+        }
+        return false;
+    }
+};
