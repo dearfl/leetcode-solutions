fb246e393ca8d1767b1585f010656b09ee7d46e1
dearfl <i@flr.me>
2022-08-02 17:09:38 +0800

    Add 1648 Sell Diminishing Valued Colored Balls

---
+++ src/1648-sell-diminishing-valued-colored-balls.cpp
@@ -0,0 +1,56 @@
+/*
+You have an inventory of different colored balls, and there is a customer that wants orders balls of any color.
+
+The customer weirdly values the colored balls. Each colored ball's value is the number of balls of that color you currently have in your inventory. For example, if you own 6 yellow balls, the customer would pay 6 for the first yellow ball. After the transaction, there are only 5 yellow balls left, so the next yellow ball is then valued at 5 (i.e., the value of the balls decreases as you sell more to the customer).
+
+You are given an integer array, inventory, where inventory[i] represents the number of balls of the ith color that you initially own. You are also given an integer orders, which represents the total number of balls that the customer wants. You can sell the balls in any order.
+
+Return the maximum total value that you can attain after selling orders colored balls. As the answer may be too large, return it modulo 10^9 + 7.
+
+Example 1:
+    Input: inventory = [2,5], orders = 4
+    Output: 14
+    Explanation: Sell the 1st color 1 time (2) and the 2nd color 3 times (5 + 4 + 3).
+    The maximum total value is 2 + 5 + 4 + 3 = 14.
+Example 2:
+    Input: inventory = [3,5], orders = 6
+    Output: 19
+    Explanation: Sell the 1st color 2 times (3 + 2) and the 2nd color 4 times (5 + 4 + 3 + 2).
+    The maximum total value is 3 + 2 + 5 + 4 + 3 + 2 = 19.
+
+Constraints:
+    1 <= inventory.length <= 10^5
+    1 <= inventory[i] <= 10^9
+    1 <= orders <= min(sum(inventory[i]), 10^9)
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& b, int orders) {
+        long long res = 0, mod = 1e9 + 7, i = 1;
+        sort(b.begin(), b.end(), std::greater<int>());
+        long long high = b[0], low = b[0];
+        while (orders) {
+            while (i < b.size() && b[i] == b[i - 1]) {
+                i++;
+            }
+            low = b.size() == i ? 0 : b[i];
+            if (orders >= (high - low) * i) {
+                orders -= (high - low) * i;
+                res = (res + (high + low + 1) * (high - low) * i / 2) % mod;
+                // cout << high << ' ' << low << ' ' << i << endl;
+                high = low;
+            } else {
+                int k = orders / i * i;
+                orders -= k;
+                low = high - k / i;
+                // cout << high << ' ' << low << ' ' << i << ' ' << k << ' ' << orders << endl;
+                res = (res + (high + low + 1) * (high - low) * i / 2) % mod;
+                res = (res + low * orders) % mod;
+                orders = 0;
+            }
+            i++;
+        }
+        return res;
+    }
+};
