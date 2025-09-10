2b77a9d96792b16dde46b9492e12484943238986
dearfl <i@flr.me>
2025-09-10 18:09:18 +0800

    Add 56 Merge Intervals rust version

---
+++ src/0056-merge-intervals.rs
@@ -0,0 +1,42 @@
+/*
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+    Output: [[1,6],[8,10],[15,18]]
+    Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+    Input: intervals = [[1,4],[4,5]]
+    Output: [[1,5]]
+    Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Constraints:
+    1 <= intervals.length <= 10^4
+    intervals[i].length == 2
+    0 <= starti <= endi <= 10^4
+*/
+
+impl Solution {
+    pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut ret = vec![];
+        intervals.sort();
+        for interval in intervals {
+            match ret.last_mut() {
+                None => {
+                    // the first interval
+                    ret.push(interval);
+                }
+                Some(ref mut last) => {
+                    if last[1] < interval[0] {
+                        // non overlapping
+                        ret.push(interval);
+                    } else {
+                        // overlapping, merge
+                        last[1] = std::cmp::max(last[1], interval[1]);
+                    }
+                }
+            }
+        }
+        ret
+    }
+}
